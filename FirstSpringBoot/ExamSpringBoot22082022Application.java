a868c52aa890d2821e5dd89d44f585034f813b22
SakshiDhote22 <110605988+SakshiDhote22@users.noreply.github.com>
2022-08-22 09:58:38 +0530

    Add files via upload

---
+++ FirstSpringBoot/ExamSpringBoot22082022Application.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamSpringBoot22082022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamSpringBoot22082022Application.class, args);
+		System.out.println("Customer.......................");
+	}
+
+}
