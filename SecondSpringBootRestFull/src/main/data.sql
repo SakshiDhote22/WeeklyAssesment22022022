5452b6cf972f6cf6fa73721d70100a242d0b2834
SakshiDhote22 <110605988+SakshiDhote22@users.noreply.github.com>
2022-08-22 10:57:18 +0530

    Add files via upload

---
+++ SecondSpringBootRestFull/src/main/data.sql
@@ -0,0 +1,5 @@
+insert into customer values (101,'Nagpur','Sakshi','Sakshi','123','9579548887');
+insert into customer values (102,'Nagpur','Sahil','Sahil','456','7038430060');
+insert into customer values (103,'Nagpur','Manthan','Manthan','789','8806229457');
+insert into customer values (104,'Nagpur','Mayank','Mayank','101','7038430060');
+insert into customer values (105,'Nagpur','Sanjay','Sanjay','111','8806229457');
